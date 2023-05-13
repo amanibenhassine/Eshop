5067ca8175e6eb75d9ea3c64e571537de414cb42
amani benhassine <amanibenhassine23@gmail.com>
2023-05-13 16:41:05 +0000

    step 1

--- frontend/app/src/components/Navbar.jsx
+++ frontend/app/src/components/Navbar.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import Home from './Home'
+import About from './About'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <div className="navbar-brand" >Navbar</div>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <Link to="/Home" className="nav-link active" aria-current="page">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link  to="/About" className="nav-link" >About</Link>
+        </li>
+      
+        
+      </ul>
+     
+    </div>
+  </div>
+</nav>  )
 }
 
 export default Navbar
