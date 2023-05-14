22b6a9d548b89ecebab4f6c18fe689a67be20925
amani benhassine <amanibenhassine23@gmail.com>
2023-05-14 19:26:34 +0000

    step 5

--- frontend/app/src/components/Produits.jsx
+++ frontend/app/src/components/Produits.jsx
@@ -1,36 +1,68 @@
 import React from 'react'
 import Card from "./Card"
-import casque from '../assets/casque.png'
 import "./Produits.css"
+import smartwatch from '../assets/smartwatch.jpg'
+import Samsung_Galaxy_Z from '../assets/Samsung_Galaxy_Z.png'
+import phone2 from '../assets/phone2.png'
+import Ordinateur from '../assets/Ordinateur.png'
+import MSI_PC_Portable_Gamer from '../assets/MSI_PC_Portable_Gamer.png'
+import MSI_PC_Gamer from '../assets/MSI_PC_Gamer.png'
+import kwmobile from '../assets/kwmobile.png'
+import iPhone_13 from '../assets/iPhone_13.png'
+import internet from '../assets/Internet.png'
+import HP_PC_Gamer from '../assets/HP_PC_Gamer.png'
+import Headphones from '../assets/Headphones.png'
+import ecouterblanch from '../assets/ecouterblanch.png'
+
+
+
+
 
 const data=[{
-    image:casque,
-    title:'casque'
+    image:ecouterblanch,
+    title:'ecouterblanch'
 },
 {
-    image:casque,
+    image:Headphones,
     title:'casque2'
 },
 {
-    image:casque,
+    image:HP_PC_Gamer,
     title:'casque3'
 },
 {
-  image:casque,
+  image:internet,
   title:'casque3'
 },
 {
-  image:casque,
+  image:iPhone_13,
   title:'casque3'
 },
 {
-  image:casque,
+  image:kwmobile,
   title:'casque3'
 },{
-  image:casque,
+  image:MSI_PC_Gamer,
   title:'casque3'
-},{
-  image:casque,
+},
+{
+  image: MSI_PC_Portable_Gamer,
+  title:'casque3'
+},
+{
+  image:Ordinateur,
+  title:'casque3'
+},
+{
+  image:phone2,
+  title:'casque3'
+},
+{
+  image:Samsung_Galaxy_Z,
+  title:'casque3'
+},
+{
+  image:smartwatch,
   title:'casque3'
 },
 
