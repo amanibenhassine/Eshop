f09ce97a712bb98843d9e63d383457663229717e
amani benhassine <amanibenhassine23@gmail.com>
2023-05-15 14:25:57 +0000

    home7

--- frontend/app/src/components/Card.jsx
+++ frontend/app/src/components/Card.jsx
@@ -4,7 +4,9 @@ import "./Card.css"
 function Card({imageItem,title}) {
   return (
     <div className='Card'>
+      <div className='cardImage'>
         <img src={imageItem} alt='img'/>
+        </div>
         <div className='card-content'>
             <h2>{title}</h2>
             <button btn btn-primary>Add to card</button>
