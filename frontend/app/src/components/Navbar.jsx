7020c217ad79c765e3c80de8870d645224a3385b
amani benhassine <amanibenhassine23@gmail.com>
2023-05-15 10:48:44 +0000

    step navbar

--- frontend/app/src/components/Navbar.jsx
+++ frontend/app/src/components/Navbar.jsx
@@ -2,30 +2,27 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import Home from './Home'
 import About from './About'
+import "./Navbar.css"
 
 function Navbar() {
   return (
-<nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <div className="navbar-brand" >Navbar</div>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <Link to="/Home" className="nav-link active" aria-current="page">Home</Link>
-        </li>
-        <li className="nav-item">
-          <Link  to="/About" className="nav-link" >About</Link>
-        </li>
-      
+<div className='navbar'>    
         
-      </ul>
-     
-    </div>
-  </div>
-</nav>  )
+        <div className='logo'><h3>Amen<span>!</span></h3></div>
+        <div> 
+         
+          <div className='navItem'>
+          <div className='navItem-1'>
+           <Link to="/Home">Home</Link>
+           </div>
+           <div className='navItem-1'>
+           <Link to="/About">About</Link>
+           </div>
+           </div>
+         </div>
+   
+       </div>
+)
 }
 
 export default Navbar
